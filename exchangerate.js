a71899c955217477014a09104f8fa5c62f017ea5
Joseph Lukudu <josephlukudu22@gmail.com>
2026-02-15 02:34:53 +0300

    change to the currency rate

--- exchangerate.js
+++ exchangerate.js
@@ -1,17 +1,13 @@
-let primarykeyforapi ='117483d04f4f4e4e825a813dc24b9c55';
+const abstractapi={
+    key: `117483d04f4f4e4e825a813dc24b9c55`,
+    baseurl: `https://exchange-rates.abstractapi.com`,
+    currency: function(){
+        return `${this.baseurl}/v1/live/?api_key=${this.key}`
+
+        
+    }
+}
 
 
 
-              function httpGetAsync(url, callback) {
-                  const xmlHttp = new XMLHttpRequest();
-                  xmlHttp.onreadystatechange = function() {
-                      if (xmlHttp.readyState === 4 && xmlHttp.status === 200)
-                      callback(xmlHttp.responseText);
-                  }
-                  xmlHttp.open("GET", url, true); // true for asynchronous
-                  xmlHttp.send(null);
-              }
-
-              const url = "https://exchange-rates.abstractapi.com/v1/live/?api_key=117483d04f4f4e4e825a813dc24b9c55&base=USD&target=EUR"
-
-              httpGetAsync(url)
+             
