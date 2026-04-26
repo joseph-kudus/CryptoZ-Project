e7e028c00ca0d40809a1288c3077a6fa330af78a
Joseph Lukudu <josephlukudu22@gmail.com>
2026-04-26 20:25:39 +0300

    fixed the bugs in footer

--- exchangerate.js
+++ exchangerate.js
@@ -1,247 +1,251 @@
-
 let country_list = {
-    AED: "AE",
-    AFN: "AF",
-    XCD: "AG",
-    ALL: "AL",
-    AMD: "AM",
-    ANG: "AN",
-    AOA: "AO",
-    AQD: "AQ",
-    ARS: "AR",
-    AUD: "AU",
-    AZN: "AZ",
-    BAM: "BA",
-    BBD: "BB",
-    BDT: "BD",
-    XOF: "BE",
-    BGN: "BG",
-    BHD: "BH",
-    BIF: "BI",
-    BMD: "BM",
-    BND: "BN",
-    BOB: "BO",
-    BRL: "BR",
-    BSD: "BS",
-    NOK: "BV",
-    BWP: "BW",
-    BYR: "BY",
-    BZD: "BZ",
-    CAD: "CA",
-    CDF: "CD",
-    XAF: "CF",
-    CHF: "CH",
-    CLP: "CL",
-    CNY: "CN",
-    COP: "CO",
-    CRC: "CR",
-    CUP: "CU",
-    CVE: "CV",
-    CYP: "CY",
-    CZK: "CZ",
-    DJF: "DJ",
-    DKK: "DK",
-    DOP: "DO",
-    DZD: "DZ",
-    ECS: "EC",
-    EEK: "EE",
-    EGP: "EG",
-    ETB: "ET",
-    EUR: "FR",
-    FJD: "FJ",
-    FKP: "FK",
-    GBP: "GB",
-    GEL: "GE",
-    GGP: "GG",
-    GHS: "GH",
-    GIP: "GI",
-    GMD: "GM",
-    GNF: "GN",
-    GTQ: "GT",
-    GYD: "GY",
-    HKD: "HK",
-    HNL: "HN",
-    HRK: "HR",
-    HTG: "HT",
-    HUF: "HU",
-    IDR: "ID",
-    ILS: "IL",
-    INR: "IN",
-    IQD: "IQ",
-    IRR: "IR",
-    ISK: "IS",
-    JMD: "JM",
-    JOD: "JO",
-    JPY: "JP",
-    KES: "KE",
-    KGS: "KG",
-    KHR: "KH",
-    KMF: "KM",
-    KPW: "KP",
-    KRW: "KR",
-    KWD: "KW",
-    KYD: "KY",
-    KZT: "KZ",
-    LAK: "LA",
-    LBP: "LB",
-    LKR: "LK",
-    LRD: "LR",
-    LSL: "LS",
-    LTL: "LT",
-    LVL: "LV",
-    LYD: "LY",
-    MAD: "MA",
-    MDL: "MD",
-    MGA: "MG",
-    MKD: "MK",
-    MMK: "MM",
-    MNT: "MN",
-    MOP: "MO",
-    MRO: "MR",
-    MTL: "MT",
-    MUR: "MU",
-    MVR: "MV",
-    MWK: "MW",
-    MXN: "MX",
-    MYR: "MY",
-    MZN: "MZ",
-    NAD: "NA",
-    XPF: "NC",
-    NGN: "NG",
-    NIO: "NI",
-    NPR: "NP",
-    NZD: "NZ",
-    OMR: "OM",
-    PAB: "PA",
-    PEN: "PE",
-    PGK: "PG",
-    PHP: "PH",
-    PKR: "PK",
-    PLN: "PL",
-    PYG: "PY",
-    QAR: "QA",
-    RON: "RO",
-    RSD: "RS",
-    RUB: "RU",
-    RWF: "RW",
-    SAR: "SA",
-    SBD: "SB",
-    SCR: "SC",
-    SDG: "SD",
-    SEK: "SE",
-    SGD: "SG",
-    SKK: "SK",
-    SLL: "SL",
-    SOS: "SO",
-    SRD: "SR",
-    STD: "ST",
-    SVC: "SV",
-    SYP: "SY",
-    SZL: "SZ",
-    THB: "TH",
-    TJS: "TJ",
-    TMT: "TM",
-    TND: "TN",
-    TOP: "TO",
-    TRY: "TR",
-    TTD: "TT",
-    TWD: "TW",
-    TZS: "TZ",
-    UAH: "UA",
-    UGX: "UG",
-    USD: "US",
-    UYU: "UY",
-    UZS: "UZ",
-    VEF: "VE",
-    VND: "VN",
-    VUV: "VU",
-    YER: "YE",
-    ZAR: "ZA",
-    ZMK: "ZM",
-    ZWD: "ZW",
+  AED: "AE",
+  AFN: "AF",
+  XCD: "AG",
+  ALL: "AL",
+  AMD: "AM",
+  ANG: "AN",
+  AOA: "AO",
+  AQD: "AQ",
+  ARS: "AR",
+  AUD: "AU",
+  AZN: "AZ",
+  BAM: "BA",
+  BBD: "BB",
+  BDT: "BD",
+  XOF: "BE",
+  BGN: "BG",
+  BHD: "BH",
+  BIF: "BI",
+  BMD: "BM",
+  BND: "BN",
+  BOB: "BO",
+  BRL: "BR",
+  BSD: "BS",
+  NOK: "BV",
+  BWP: "BW",
+  BYR: "BY",
+  BZD: "BZ",
+  CAD: "CA",
+  CDF: "CD",
+  XAF: "CF",
+  CHF: "CH",
+  CLP: "CL",
+  CNY: "CN",
+  COP: "CO",
+  CRC: "CR",
+  CUP: "CU",
+  CVE: "CV",
+  CYP: "CY",
+  CZK: "CZ",
+  DJF: "DJ",
+  DKK: "DK",
+  DOP: "DO",
+  DZD: "DZ",
+  ECS: "EC",
+  EEK: "EE",
+  EGP: "EG",
+  ETB: "ET",
+  EUR: "FR",
+  FJD: "FJ",
+  FKP: "FK",
+  GBP: "GB",
+  GEL: "GE",
+  GGP: "GG",
+  GHS: "GH",
+  GIP: "GI",
+  GMD: "GM",
+  GNF: "GN",
+  GTQ: "GT",
+  GYD: "GY",
+  HKD: "HK",
+  HNL: "HN",
+  HRK: "HR",
+  HTG: "HT",
+  HUF: "HU",
+  IDR: "ID",
+  ILS: "IL",
+  INR: "IN",
+  IQD: "IQ",
+  IRR: "IR",
+  ISK: "IS",
+  JMD: "JM",
+  JOD: "JO",
+  JPY: "JP",
+  KES: "KE",
+  KGS: "KG",
+  KHR: "KH",
+  KMF: "KM",
+  KPW: "KP",
+  KRW: "KR",
+  KWD: "KW",
+  KYD: "KY",
+  KZT: "KZ",
+  LAK: "LA",
+  LBP: "LB",
+  LKR: "LK",
+  LRD: "LR",
+  LSL: "LS",
+  LTL: "LT",
+  LVL: "LV",
+  LYD: "LY",
+  MAD: "MA",
+  MDL: "MD",
+  MGA: "MG",
+  MKD: "MK",
+  MMK: "MM",
+  MNT: "MN",
+  MOP: "MO",
+  MRO: "MR",
+  MTL: "MT",
+  MUR: "MU",
+  MVR: "MV",
+  MWK: "MW",
+  MXN: "MX",
+  MYR: "MY",
+  MZN: "MZ",
+  NAD: "NA",
+  XPF: "NC",
+  NGN: "NG",
+  NIO: "NI",
+  NPR: "NP",
+  NZD: "NZ",
+  OMR: "OM",
+  PAB: "PA",
+  PEN: "PE",
+  PGK: "PG",
+  PHP: "PH",
+  PKR: "PK",
+  PLN: "PL",
+  PYG: "PY",
+  QAR: "QA",
+  RON: "RO",
+  RSD: "RS",
+  RUB: "RU",
+  RWF: "RW",
+  SAR: "SA",
+  SBD: "SB",
+  SCR: "SC",
+  SDG: "SD",
+  SEK: "SE",
+  SGD: "SG",
+  SKK: "SK",
+  SLL: "SL",
+  SOS: "SO",
+  SRD: "SR",
+  STD: "ST",
+  SVC: "SV",
+  SYP: "SY",
+  SZL: "SZ",
+  THB: "TH",
+  TJS: "TJ",
+  TMT: "TM",
+  TND: "TN",
+  TOP: "TO",
+  TRY: "TR",
+  TTD: "TT",
+  TWD: "TW",
+  TZS: "TZ",
+  UAH: "UA",
+  UGX: "UG",
+  USD: "US",
+  UYU: "UY",
+  UZS: "UZ",
+  VEF: "VE",
+  VND: "VN",
+  VUV: "VU",
+  YER: "YE",
+  ZAR: "ZA",
+  ZMK: "ZM",
+  ZWD: "ZW",
 };
 
-// api key from exhange rate api
- let apiKey = "ebf4f2f58179610cdf576a91";
-//let api_key = "55ece8286efe464654b8d1bf";
+// api key from exchange rate api
+let api_key = "ebf4f2f58179610cdf576a91";
 
 const dropList = document.querySelectorAll("form select");
 const fromCurrency = document.querySelector(".from select");
 const toCurrency = document.querySelector(".to select");
-const getBtn = document.querySelector("form button");
-const amount = document.querySelector("form input");
-const exchangeRateText = document.querySelector("form .exchange-rate");
-const exhangeIcon = document.querySelector("form .icon");
+const getBtn = document.querySelector(".exchange-rate button");
+const amount = document.querySelector(".from input");
+const exchangeRateText = document.querySelector(".exchange-rate button");
+const exchangeIcon = document.querySelector("form .icon");
 
 for (let i = 0; i < dropList.length; i++) {
-    for (let currency_code in country_list) {
-        let selected =
-            i == 0
-                ? currency_code == "USD"
-                    ? "selected"
-                    : ""
-                : currency_code == "UGX"
-                ? "selected"
-                : "";
-        let optionTag = `<option value="${currency_code}" ${selected}>${currency_code}</option>`;
-        dropList[i].insertAdjacentHTML("beforeend", optionTag);
-    }
-    dropList[i].addEventListener("change", (e) => {
-        loadFlag(e.target);
-    });
+  for (let currency_code in country_list) {
+    let selected =
+      i == 0
+        ? currency_code == "USD"
+          ? "selected"
+          : ""
+        : currency_code == "UGX"
+          ? "selected"
+          : "";
+    let optionTag = `<option value="${currency_code}" ${selected}>${currency_code}</option>`;
+    dropList[i].insertAdjacentHTML("beforeend", optionTag);
+  }
+  dropList[i].addEventListener("change", (e) => {
+    loadFlag(e.target);
+    getExchangeRate();
+  });
 }
 
 // Load the selected flag
 const loadFlag = (element) => {
-    for (let code in country_list) {
-        if (code == element.value) {
-            let imgTag = element.parentElement.querySelector("img");
-            imgTag.src = `https://flagcdn.com/48x36/${country_list[
-                code
-            ].toLowerCase()}.png`;
-        }
+  for (let code in country_list) {
+    if (code == element.value) {
+      let imgTag = element.parentElement.querySelector("img");
+      imgTag.src = `https://flagcdn.com/48x36/${country_list[code].toLowerCase()}.png`;
     }
+  }
 };
 
 // Get the exchange rate
 const getExchangeRate = () => {
-    let amountVal = amount.value;
-    if (amountVal == "" || amountVal == "0") {
-        amount.value = "1";
-        amountVal = 1;
-    }
+  let amountVal = amount.value;
+  if (amountVal == "" || amountVal == "0") {
+    amount.value = "1";
+    amountVal = 1;
+  }
 
-    exchangeRateText.innerText = "EUR GBP BTC";
+  exchangeRateText.innerText = "Loading...";
 
-    let url = `https://v6.exchangerate-api.com/v6/${api_key}/latest/${fromCurrency.value}`;
-    fetch(url)
-        .then((response) => response.json())
-        .then((result) => {
-            let exchangeRate = result.conversion_rates[toCurrency.value];
-            let totalExRate = (amountVal * exchangeRate).toFixed(2);
-            exchangeRateText.innerText = `${amountVal} ${fromCurrency.value} = ${totalExRate} ${toCurrency.value}`;
-        })
-        .catch(() => {
-            exchangeRateText.innerText = `Something went wrong`;
-        });
+  let url = `https://v6.exchangerate-api.com/v6/${api_key}/latest/${fromCurrency.value}`;
+  fetch(url)
+    .then((response) => response.json())
+    .then((result) => {
+      if (result.result === "success") {
+        let exchangeRate = result.conversion_rates[toCurrency.value];
+        let totalExRate = (amountVal * exchangeRate).toFixed(4);
+        exchangeRateText.innerText = `${amountVal} ${fromCurrency.value} = ${totalExRate} ${toCurrency.value}`;
+      } else {
+        exchangeRateText.innerText = `API Error`;
+      }
+    })
+    .catch(() => {
+      exchangeRateText.innerText = `Something went wrong`;
+    });
 };
 
 // Event Listeners
 window.addEventListener("load", () => {
-    getExchangeRate();
+  loadFlag(fromCurrency);
+  loadFlag(toCurrency);
+  getExchangeRate();
 });
 
 getBtn.addEventListener("click", (e) => {
-    e.preventDefault();
-    getExchangeRate();
+  e.preventDefault();
+  getExchangeRate();
 });
 
-exhangeIcon.addEventListener("click", () => {
-    let tempCode = fromCurrency.value;
-    fromCurrency.value = toCurrency.value;
-    toCurrency.value = tempCode;
-
-    loadFlag(fromCurrency);
-    loadFlag(toCurrency);
-    getExchangeRate();
+exchangeIcon.addEventListener("click", () => {
+  let tempCode = fromCurrency.value;
+  fromCurrency.value = toCurrency.value;
+  toCurrency.value = tempCode;
+  loadFlag(fromCurrency);
+  loadFlag(toCurrency);
+  getExchangeRate();
 });
+
+amount.addEventListener("input", getExchangeRate);
